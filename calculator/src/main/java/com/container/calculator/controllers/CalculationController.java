37ba9f8679be863f94dec04fb4a7f368c333af70
RomuloJB <jordaobogoni@gmail.com>
2025-12-29 23:35:01 -0300

    first commit

---
+++ calculator/src/main/java/com/container/calculator/controllers/CalculationController.java
@@ -0,0 +1,5 @@
+package com.container.calculator.controllers;
+
+public class CalculationController {
+    
+}
---
+++ calculator/src/main/java/com/container/calculator/models/Calculation.java
@@ -0,0 +1,5 @@
+package com.container.calculator.models;
+
+public class Calculation {
+    
+}
